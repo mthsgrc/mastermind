c0e4f47853efd019353a9e35564fa8256c9aca65
mthsgrc <matheus.dev.garcia@gmail.com>
2020-11-05 19:37:14 -0300

    initial

---
+++ mastermind.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+require "pry"
+
+
+
+class DecodingBoard
+
+end
+
+class CodePegs
+
+end
+
+class KeyPegs
+
+end
+
+class CodeMaker
+
+end
+
+class CodeBreaker
+
+end
+
+
+
+system "clear"
