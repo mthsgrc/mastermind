7431b3a766f4693879faa9aa24a01988626838ea
mthsgrc <matheus.dev.garcia@gmail.com>
2020-11-06 19:48:16 -0300

    add string_mod.rb files. To change colors of pegs

---
+++ string_mod.rb
@@ -0,0 +1,9 @@
+
+class String
+  def gray;           "\e[37m#{self}\e[0m" end
+  def red;            "\e[31m#{self}\e[0m" end
+  def green;          "\e[32m#{self}\e[0m" end
+  def brown;          "\e[33m#{self}\e[0m" end
+  def blue;           "\e[34m#{self}\e[0m" end
+  def magenta;        "\e[35m#{self}\e[0m" end
+end
