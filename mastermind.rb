182e22b94bf68531c6afb88d9f2ef86422449317
mthsgrc <matheus.dev.garcia@gmail.com>
2020-11-06 21:53:39 -0300

    fixed generate secret. Adjust color of Pegs.

--- mastermind.rb
+++ mastermind.rb
@@ -7,20 +7,19 @@ require_relative "./string_mod.rb"
 class DecodingBoard
   attr_accessor :board
 
-  def board=(x,y,value)
-    self[x][y] = value
-
+  def initialize
+    @board = Array.new(12) { Array.new(4) { "_" } }
+    create_board
   end
 
-  def initialize
-    @board = Array.new(12) { Array.new(4) { "|__" } }
-    create_board
+  def board=(x,y,value)
+    self[x][y] = value
   end
 
   def create_board
     for line in @board
       for spot in line
-        print spot
+        print "|_#{spot}"
       end
       puts "|"
     end
@@ -29,50 +28,96 @@ class DecodingBoard
   def draw_board
     for line in @board
       for spot in line
-        print spot
+        print "| #{spot}"
       end
       puts "|"
     end
   end
 end
 
-class CodePegs
+class Pegs
+  BLUE = "● ".blue
+  GRAY = "● ".gray
+  RED = "● ".red
+  GREEN = "● ".green
+  BROWN = "● ".brown
+  MAGENTA = "● ".magenta
+  WHITE = "● ".white
+end
+
+class CodePegs < Pegs
+
 
 end
 
 
-class KeyPegs
+class KeyPegs < Pegs
 
 end
 
 
-class CodeMaker
+class CodeMaker < Pegs
   attr_reader :secret
-
-  def initialize(name = "CPU", secret)
+  # again player or CPU
+  # @secret = generate_secret
+  # def initialize(name = "CPU", secret)
+  def initialize(name = "CPU")
     @name = name
-    @secret = secret
+    @secret = generate_secret
+
   end
 
   def creat_secret
 
   end
+
+  def generate_secret
+    # blue = "● ".blue
+    # gray = "● ".gray
+    # red = "● ".red
+    # green = "● ".green
+    # brown = "● ".brown
+    # magenta = "● ".magenta
+
+    secret = []
+    while secret.length < 4
+      secret << [BLUE, GRAY, RED, GREEN, BROWN, MAGENTA].sample
+    end
+
+    secret
+  end
+
 end
 
 class CodeBreaker
+  attr_accessor :break_guess
   def initialize(name)
     @name = name
+  end
 
+  def guess
+    guess = []
+    print "Insert your guess. Choose 4 option between (B)lue, (G)ray, (R)ed, Gr(E)en, Br(O)wn and (M)agenta:"
+    binding.pry
+    until guess.length == 4
+      single_guess = gets.chomp
+      
+
+
+    end
+  end
+
+  def valid_guess?(guess)
+    guess.to_s.upcase!
+    ["B","G","R","E","O","M"].include?(guess) ? true : false
   end
 end
 
 class Match
   attr_accessor :board
-  attr_reader :secret
 
   def initialize
-    @secret = generate_secret
-    @codemaker = CodeMaker.new("CPU", @secret)
+    @codemaker = CodeMaker.new
 
     print "Insert name of Player: "
     codebreaker_name = gets.chomp
@@ -80,24 +125,20 @@ class Match
 
     @play_board = DecodingBoard.new
 
-    binding.pry
+    match_starts
   end
 
-
-  def generate_secret
-    blue = "● ".blue
-    gray = "● ".gray
-    red = "● ".red
-    green = "● ".green
-    brown = "● ".brown
-    magenta = "● ".magenta
-
-    secret = [blue, gray, red, green, brown, magenta].permutation(4).to_a.sample
-    secret
-
+  def match_starts
+    until break_code(@codemaker.secret, @codebreaker.break_guess)
+      @codebreaker.guess
+    end
   end
+
+  def break_code(secret, guess)
+    secret == guess ? true : false
+  end
+
 end
-
 Match.new
 
 
