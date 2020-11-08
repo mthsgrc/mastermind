0e48eb6b1fcc74f720cc3bbecec996459c8f9cce
mthsgrc <matheus.dev.garcia@gmail.com>
2020-11-08 18:51:04 -0300

    Module CheckGuess. Record breaker guess. Compare to secret, return ok if same.

--- mastermind.rb
+++ mastermind.rb
@@ -3,6 +3,20 @@ require "pry"
 
 require_relative "./string_mod.rb"
 
+module CheckGuess
+  def valid_guess?(guess)
+    # binding.pry
+    guess.to_s.upcase!
+    if ["B","G","R","E","O","M"].include?(guess)
+      return true
+    else
+      puts "Not a valid guess. Insert a valid guess:"
+      puts "(B)lue, (G)ray, (R)ed, Gr(E)en, Br(O)wn and (M)agenta:"
+      return false
+    end
+  end
+end
+
 
 class DecodingBoard
   attr_accessor :board
@@ -18,21 +32,23 @@ class DecodingBoard
 
   def create_board
     for line in @board
-      for spot in line
-        print "|_#{spot}"
+      for holes in line
+        print "|_#{holes}"
       end
       puts "|"
     end
   end
 
   def draw_board
+    puts
     for line in @board
-      for spot in line
-        print "| #{spot}"
+      for holes in line
+        print "|#{holes}"
       end
       puts "|"
     end
   end
+  
 end
 
 class Pegs
@@ -43,6 +59,20 @@ class Pegs
   BROWN = "● ".brown
   MAGENTA = "● ".magenta
   WHITE = "● ".white
+  BLACK = "● ".black
+
+  def guess_to_peg(guess)
+    case guess
+    when "B" then guess = BLUE
+    when "G" then guess = GRAY
+    when "R" then guess = RED
+    when "E" then guess = GREEN
+    when "O" then guess = BROWN
+    when "M" then guess = MAGENTA
+    end
+
+    guess
+  end
 end
 
 class CodePegs < Pegs
@@ -57,7 +87,9 @@ end
 
 
 class CodeMaker < Pegs
+  include CheckGuess
   attr_reader :secret
+
   # again player or CPU
   # @secret = generate_secret
   # def initialize(name = "CPU", secret)
@@ -68,29 +100,27 @@ class CodeMaker < Pegs
   end
 
   def creat_secret
-
+    secret = []
+    until secret.length == 4
+      secret << gets.chomp if valid_guess?(secret)
+    end
+    secret
   end
 
   def generate_secret
-    # blue = "● ".blue
-    # gray = "● ".gray
-    # red = "● ".red
-    # green = "● ".green
-    # brown = "● ".brown
-    # magenta = "● ".magenta
-
     secret = []
     while secret.length < 4
       secret << [BLUE, GRAY, RED, GREEN, BROWN, MAGENTA].sample
     end
-
     secret
   end
 
 end
 
-class CodeBreaker
+class CodeBreaker < Pegs
+  include CheckGuess
   attr_accessor :break_guess
+
   def initialize(name)
     @name = name
   end
@@ -98,23 +128,26 @@ class CodeBreaker
   def guess
     guess = []
     print "Insert your guess. Choose 4 option between (B)lue, (G)ray, (R)ed, Gr(E)en, Br(O)wn and (M)agenta:"
-    binding.pry
+    # binding.pry
     until guess.length == 4
       single_guess = gets.chomp
-      
-
-
+      if valid_guess?(single_guess)
+        single_guess = guess_to_peg(single_guess)
+        guess << single_guess
+        for i in guess
+          print "#{i} "
+        end
+        print "Insert another guess:" if guess.length < 4
+      end
     end
+    @break_guess = guess
+    guess
   end
 
-  def valid_guess?(guess)
-    guess.to_s.upcase!
-    ["B","G","R","E","O","M"].include?(guess) ? true : false
-  end
 end
 
 class Match
-  attr_accessor :board
+  attr_accessor :play_board
 
   def initialize
     @codemaker = CodeMaker.new
@@ -125,28 +158,61 @@ class Match
 
     @play_board = DecodingBoard.new
 
-    match_starts
+    start_match
   end
 
-  def match_starts
-    until break_code(@codemaker.secret, @codebreaker.break_guess)
-      @codebreaker.guess
+  def start_match
+    @turns = 1
+    break_guess = @codebreaker.guess
+
+    while @turns < 3
+      @play_board.board[@turns-1]= break_guess
+      @play_board.draw_board
+
+      if code_breaked?(@codemaker.secret, break_guess)
+        winner_message
+      else
+        binding.pry
+        puts
+        puts "Guess does not match the secret. Try Again.\n"
+        puts
+        break_guess = @codebreaker.guess
+        @turns += 1
+      end
     end
+    lost_message
   end
 
-  def break_code(secret, guess)
+  def code_breaked?(secret, guess)
     secret == guess ? true : false
   end
 
+  def winner_message
+    binding.pry
+    puts
+    puts
+    puts "Code Cracked!"
+    print "SECRET > "
+    for i in @codemaker.secret
+      print "#{i} "
+    end
+    puts
+    print "GUESS  > "
+    for i in @codebreaker.break_guess
+      print "#{i} "
+    end
+    puts
+  end
+
+  def lost_message
+    puts
+    puts "You couldn't crack the code."
+  end
 end
+
+
+
 Match.new
 
 
 # system "clear"
-
-# red
-# blue
-# yellow
-# purple
-# black
-# white
